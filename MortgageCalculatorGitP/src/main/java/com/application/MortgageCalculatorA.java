2cca7a3af62779c00763f44acfe27e85d0020e97
Nelosh <151106526+nssyakoubi@users.noreply.github.com>
2024-01-22 23:10:40 -0500

    commit new nella

--- MortgageCalculatorGitP/src/main/java/com/application/MortgageCalculatorA.java
+++ MortgageCalculatorGitP/src/main/java/com/application/MortgageCalculatorA.java
@@ -7,5 +7,6 @@ public class MortgageCalculatorA {
 	public static void main(String[] args) {
 		System.out.println("Motgagae Calculator is starting ");
 		System.out.println("Motgagae Calculator is ending ");
+		System.out.println("Motgagae Calculator is ending ");
 	}
 }
