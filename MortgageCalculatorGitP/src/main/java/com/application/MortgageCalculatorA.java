003beda48ef9b1b264cef6fecf4c43b3e89e6cc5
Nelosh <151106526+nssyakoubi@users.noreply.github.com>
2024-01-22 23:25:03 -0500

    another Hamzalolo comment

--- MortgageCalculatorGitP/src/main/java/com/application/MortgageCalculatorA.java
+++ MortgageCalculatorGitP/src/main/java/com/application/MortgageCalculatorA.java
@@ -5,6 +5,10 @@ package com.application;
 public class MortgageCalculatorA {
 
 	public static void main(String[] args) {
+
+		System.out.println("Motgagae Calculator is starting ");
+		System.out.println("Motgagae Calculator is ending ");
+		System.out.println("Motgagae Calculator is ending ");
 		System.out.println("Motgagae Calculator is starting ");
 		System.out.println("Motgagae Calculator is ending ");
 		System.out.println("Motgagae Calculator is ending ");
